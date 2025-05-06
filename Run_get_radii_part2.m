b98f9b2d486ebf7000489b30387e30b59242ada3
John <millerjohn902@gmail.com>
2025-05-06 13:25:53 +0100

    Updated Documentation and minor alterations
    updated the MatlabNetworks document to introduce how to run either of the code processes. updated Run part 2 to allow for the saving of multiple types of files. Removed the MATLAB version of part 2 since the MATLAB code is not one file

--- Run_get_radii_part2.m
+++ Run_get_radii_part2.m
@@ -1,18 +1,18 @@
-%function [new_vessel_details,Data,newNetwork] = Run_get_radii_part2(Name,TaperVes,Scale,ploton)
+function [new_vessel_details,Data,newNetwork] = Run_get_radii_part2(Name,TaperVes,Scale,ploton)
 
     clear all;
     close all;
 
-    Name = 'control_3'; % name of network
-    TaperVes= []; % index of vessels to be tapered put in "XX"
-    Scale = 1;   % scaling from voxels to mm from CT image
-    ploton = 1;  % plot figures
+    %Name = 'control_3'; % name of network
+    % TaperVes= []; % index of vessels to be tapered put in "XX"
+    % Scale = 1;   % scaling from voxels to mm from CT image
+    % ploton = 1;  % plot figures
 
     sM = strcat('./Output/Segmentation_',Name,'.mat');
     sE = strcat('./Input/IMPORT_',Name,'.xlsx'); 
     sO = strcat('./FluidsInput/FluidInput_',Name,'.mat');
 
-    load(sM,'maxDaughters','vessel_details','newNetwork');
+    load(sM,'maxDaughters','vessel_details','newNetwork','arcsC3','nodesC2');
     
     [new_vessel_details] = Import_from_excel(sE,vessel_details);
     
@@ -27,5 +27,9 @@
 
     %[DataOut] = get_radii_aorta(new_vessel_details, Scale, Data);  % Only call if you want to correct radii extraction for tapering aorta.
 
-    save(sO,'Data');
-%end
+    save(sO,'Data'); %only saves the necessary input for the fluids code
+    save(strcat('Output/Vessels_',Name,'.mat')); %saves entire workspace
+    save(strcat('Networks/Network_Vessels_',Name,'.mat'), 'arcsC3', 'nodesC2', 'Data','vessel_details', 'TaperVes','ploton','Scale','maxDaughters')
+        % ^ saves the necessary data for statistic extraction and to run/work
+        % on the changepoint algorithm
+end
